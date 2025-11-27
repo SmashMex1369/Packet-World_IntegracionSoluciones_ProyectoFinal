7e63514762d51da7e5c23a39de78f02015e6d9cd
citlallixx03 <zs21016376@estudiantes.uv.mx>
2025-11-26 20:43:54 -0600

    pantalla de login

--- PacketWorldClienteEscritorio/src/packetworldclienteescritorio/FXMLInicioSesionController.java
+++ PacketWorldClienteEscritorio/src/packetworldclienteescritorio/FXMLInicioSesionController.java
@@ -2,13 +2,46 @@ package packetworldclienteescritorio;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.stage.Stage;
 
 public class FXMLInicioSesionController implements Initializable {
+
+    @FXML
+    private TextField lbNoPersonal;
+    @FXML
+    private TextField lbPassword;
+    @FXML
+    private Label lbTitulo;
+    @FXML
+    private ImageView ivImg;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+       
+    }
+
+    @FXML
+    private void btnIniciar(ActionEvent event) {
+        try {
+            FXMLLoader cargador= new FXMLLoader(getClass().getResource("FXMLMenuPrincipal.fxml"));
+            Parent vista= cargador.load();
+            //FXMLMenuPrincipalController controlador= cargador.getController();
+            Scene escena= new Scene(vista);
+            Stage escenario= (Stage) lbNoPersonal.getScene().getWindow();
+            escenario.setScene(escena);
+            escenario.setTitle("Menú Principal");
+            escenario.show();
+        } catch (Exception e) {
+        }
+    }  
 }
