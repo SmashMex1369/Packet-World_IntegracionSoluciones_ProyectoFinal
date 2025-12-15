6903a738695d5af1b7bb8baf0acb70ba9ae984b3
citlallixx03 <zs21016376@estudiantes.uv.mx>
2025-12-14 20:57:07 -0600

    Implementacion incompleta del login

--- PacketWorldClienteEscritorio/src/packetworldclienteescritorio/FXMLInicioSesionController.java
+++ PacketWorldClienteEscritorio/src/packetworldclienteescritorio/FXMLInicioSesionController.java
@@ -8,21 +8,23 @@ import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import packetworldclienteescritorio.dominio.InicioSesionImp;
+import packetworldclienteescritorio.dto.RSAutenticacionColaborador;
+import packetworldclienteescritorio.utilidad.Utilidades;
 
 public class FXMLInicioSesionController implements Initializable {
 
     @FXML
-    private TextField lbNoPersonal;
-    @FXML
-    private TextField lbPassword;
-    @FXML
-    private Label lbTitulo;
-    @FXML
     private ImageView ivImg;
+    @FXML
+    private TextField tfNoPersonal;
+    @FXML
+    private TextField tfPassword;
 
     @Override
     public void initialize(URL url, ResourceBundle rb) {
@@ -32,16 +34,40 @@ public class FXMLInicioSesionController implements Initializable {
 
     @FXML
     private void btnIniciar(ActionEvent event) {
+        
+        String noPersonal= tfNoPersonal.getText();
+        String password= tfPassword.getText();
+        
+        if(!noPersonal.isEmpty() && !password.isEmpty()){
+            verificarCredenciales(noPersonal, password);
+        }else{        
+            Utilidades.mostrarAlertaSimple("Campos requeridos", "El no. Personal y/o contraseña son obligatorios", Alert.AlertType.WARNING);
+        }
+    }
+    
+    private void verificarCredenciales(String noPersonal, String password){
+        RSAutenticacionColaborador respuesta= InicioSesionImp.verificarCredenciales(noPersonal, password);
+        if(!respuesta.isError()){
+            Utilidades.mostrarAlertaSimple("Credenciales correctas", "Bienvenido(a) colaborador(a) "+respuesta.getColaborador().getNombre()+" al sistema", Alert.AlertType.INFORMATION);
+            irMenuPrincipal();
+        }else{
+            System.out.println("error VC");
+           Utilidades.mostrarAlertaSimple("Error", respuesta.getMensaje(), Alert.AlertType.ERROR);
+        }           
+    }
+    
+    private void irMenuPrincipal(){
         try {
             FXMLLoader cargador= new FXMLLoader(getClass().getResource("FXMLMenuPrincipal.fxml"));
             Parent vista= cargador.load();
             //FXMLMenuPrincipalController controlador= cargador.getController();
             Scene escena= new Scene(vista);
-            Stage escenario= (Stage) lbNoPersonal.getScene().getWindow();
+            Stage escenario= (Stage) tfNoPersonal.getScene().getWindow();
             escenario.setScene(escena);
             escenario.setTitle("Menú Principal");
             escenario.show();
         } catch (Exception e) {
+            e.printStackTrace();
         }
-    }  
+    }
 }
