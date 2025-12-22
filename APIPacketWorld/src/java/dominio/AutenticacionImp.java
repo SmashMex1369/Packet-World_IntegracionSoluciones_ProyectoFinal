b3947d5959d0f83e237da9cadf3c31d61dc0566a
Christopher Alessandro Rodriguez Salazar <chris1203aless@gmail.com>
2025-12-21 22:53:02 -0600

    Servicio de Autenticacion de Conductores para cliente movil

--- APIPacketWorld/src/java/dominio/AutenticacionImp.java
+++ APIPacketWorld/src/java/dominio/AutenticacionImp.java
@@ -1,11 +1,14 @@
 package dominio;
 
 import dto.RSAutenticacionColaborador;
+import dto.RSAutenticacionConductor;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import modelo.mybatis.MyBatisUtil;
 import org.apache.ibatis.session.SqlSession;
 import pojo.Colaborador;
+import pojo.Conductor;
+import utilidades.Constantes;
 
 /**
  *
@@ -36,8 +39,37 @@ public class AutenticacionImp {
                 respuesta.setMensaje(e.getMessage());
             }
         }else{
-            respuesta.setMensaje("Lo sentimos, por el momento no hay conexión a la base de datos");
+            respuesta.setMensaje(Constantes.MSJ_ERROR_BD);
         }
         return respuesta;
     }
+    
+    public static RSAutenticacionConductor autenticarConductor(String noPersonal, String contraseña){
+        
+        RSAutenticacionConductor respuesta= new RSAutenticacionConductor();
+        respuesta.setError(true);
+        SqlSession conexionBD= MyBatisUtil.getSession();
+        if(conexionBD!=null){
+            try {
+                HashMap<String, String> parametros= new LinkedHashMap<>();
+                parametros.put("noPersonal", noPersonal);
+                parametros.put("contraseña", contraseña);
+                Conductor conductor = conexionBD.selectOne("autenticacion.conductor", parametros);
+                if(conductor!=null){
+                    respuesta.setError(false);
+                    respuesta.setMensaje("Credenciales correctas del conductor "+conductor.getNombre());
+                    respuesta.setConductor(conductor);
+                }else{
+                    respuesta.setMensaje("Credenciales incorrectas");
+                }    
+                conexionBD.close();
+            } catch (Exception e) {
+                respuesta.setMensaje(e.getMessage());
+            }
+        }else{
+            respuesta.setMensaje(Constantes.MSJ_ERROR_BD);
+        }
+        return respuesta;
+    }
+    
 }
